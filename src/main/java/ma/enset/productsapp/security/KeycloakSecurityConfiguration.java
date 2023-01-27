0258b755b98eb5e43b0eef6c1c5ca8347efc9caa
BEHZAD NEKOUEI <behzad.nekouei@atos.net>
2023-01-27 15:08:34 +0100

    KeycloakSecurityConfiguration class created and implemented

---
+++ src/main/java/ma/enset/productsapp/security/KeycloakSecurityConfiguration.java
@@ -0,0 +1,25 @@
+package ma.enset.productsapp.security;
+
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+
+public class KeycloakSecurityConfiguration extends KeycloakWebSecurityConfigurerAdapter {
+    @Override
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy( new SessionRegistryImpl());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(keycloakAuthenticationProvider());
+    }
+}
