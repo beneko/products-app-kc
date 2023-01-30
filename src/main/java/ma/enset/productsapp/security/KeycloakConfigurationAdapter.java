4253b8d7524cee645f2d122be7e4245854a7138d
BEHZAD NEKOUEI <behzad.nekouei@atos.net>
2023-01-30 11:58:10 +0100

    add keycloackRestTemplate to context

--- src/main/java/ma/enset/productsapp/security/KeycloakConfigurationAdapter.java
+++ src/main/java/ma/enset/productsapp/security/KeycloakConfigurationAdapter.java
@@ -2,6 +2,8 @@ package ma.enset.productsapp.security;
 
 import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.client.KeycloakClientRequestFactory;
+import org.keycloak.adapters.springsecurity.client.KeycloakRestTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,5 +13,10 @@ public class KeycloakConfigurationAdapter {
     // We use spring boot application.properties to configure keycloak instead of keycloak.json
     KeycloakSpringBootConfigResolver keycloakSpringBootConfigResolver() {
         return new KeycloakSpringBootConfigResolver();
+    }
+
+    @Bean
+    KeycloakRestTemplate keycloakRestTemplate(KeycloakClientRequestFactory KeycloakClientRequestFactory) {
+        return new KeycloakRestTemplate(KeycloakClientRequestFactory);
     }
 }
